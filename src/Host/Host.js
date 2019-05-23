08a40456716386626fbf7608983c1e3daad38947
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-23 14:48:49 -0400

    Replace class with className in Host

--- src/Host/Host.js
+++ src/Host/Host.js
@@ -6,15 +6,15 @@ export default function Host() {
 
     return (
         <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-1">
-            <h1 class="uk-heading-line uk-text-center uk-margin-large-top uk-margin-large-bottom">
+            <h1 className="uk-heading-line uk-text-center uk-margin-large-top uk-margin-large-bottom">
                 <span>Host</span>
             </h1>
 
             { Array.from(alphabets).map( (letter,i) => 
                 <button key={i} className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
-                    <div class="uk-card uk-card-secondary uk-card-body">
-                        <div class="uk-card-badge uk-label">Points: 3</div>
-                        <h3 class="uk-card-title">Story {letter}</h3>
+                    <div className="uk-card uk-card-secondary uk-card-body">
+                        <div className="uk-card-badge uk-label">Points: 3</div>
+                        <h3 className="uk-card-title">Story {letter}</h3>
                     </div>
                 </button>
                 )
@@ -22,8 +22,8 @@ export default function Host() {
 
 
                 <button className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
-                    <div class="uk-card uk-card-secondary uk-card-body">
-                        <h3 class="uk-card-title">+</h3>
+                    <div className="uk-card uk-card-secondary uk-card-body">
+                        <h3 className="uk-card-title">+</h3>
                     </div>
                 </button>
         </div>
