253e20f0148132d22e735db818a472caeab21a36
Linna Meng <mengl@mac-2387.intware.com>
2019-05-23 15:22:42 -0400

    LM update tests

--- src/Host/Host.js
+++ src/Host/Host.js
@@ -9,13 +9,12 @@ export default function Host() {
         <>
         <div className="uk-container uk-text-center@m uk-margin-top">
             <h1 className="uk-heading-small uk-heading-divider">Host</h1>
-
             <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-1">
-            <Link to="/estimate"><button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right">Estimate</button></Link>
+                <Link to="/estimate"><button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right">Estimate</button></Link>
 
                 { Array.from(alphabets).map( (letter,i) => 
-                    <button key={i} className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
-                        <div className="uk-card uk-card-secondary uk-card-body">
+                    <button key={i} className="uk-margin uk-button-primary uk-width-1-2 uk-text-large">
+                        <div className="uk-card uk-card-primary uk-card-body">
                             <div className="uk-card-badge uk-label">Points: 3</div>
                             <h3 className="uk-card-title">Story {letter}</h3>
                         </div>
@@ -23,12 +22,11 @@ export default function Host() {
                     )
                 }
 
-
-                    <button className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
-                        <div className="uk-card uk-card-secondary uk-card-body">
-                            <h3 className="uk-card-title">+</h3>
-                        </div>
-                    </button>
+                <button className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
+                    <div className="uk-card uk-card-secondary uk-card-body">
+                        <h3 className="uk-card-title">+</h3>
+                    </div>
+                </button>
             </div>
         </div>
         </>
