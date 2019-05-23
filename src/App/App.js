24bc16ed8cefd10181708f3f9dc5c9f315b32b9e
Linna Meng <mengl@mac-2387.intware.com>
2019-05-23 14:23:15 -0400

    LM fix merge

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import { HashRouter as Router, Route } from "react-router-dom";
 import React from 'react';
 import Login from './../Login/Login';
-import Rooms from './../Rooms/Rooms';
 import Host from './../Host/Host'
 import Meetings from './../Meetings/Meetings';
 
@@ -10,7 +9,6 @@ export default function App() {
   return (
     <Router>
       <Route exact path="/" component={Login}/>
-      <Route path="/rooms" component={Rooms} />
       <Route path="/host" component={Host} />
       <Route path="/meetings/" component={Meetings} />
     </Router>
