e2b48723159091e7516a89f8bf26f1629d319cd5
Ramandeep Aujla <aujlar@mac-3081.intware.com>
2019-05-23 13:57:54 -0400

    RKA extracted logic to get meeting buttons

--- src/Meetings/Meetings.js
+++ src/Meetings/Meetings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const alphabets = ['Meeting A', 'Meeting B', 'Meeting C', 'Meeting D', 'Meeting E'];
+const meetings = ['Meeting A', 'Meeting B', 'Meeting C', 'Meeting D', 'Meeting E'];
 
 export default function Meetings() {
     
@@ -8,11 +8,16 @@ export default function Meetings() {
             <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
                 <h1 className="uk-text-center uk-heading-divider">MEETINGS</h1>                
                 <div className="uk-flex uk-flex-column uk-flex-middle">
-                    { alphabets.map((letter, index) => 
-                        <button key={index} className="uk-margin uk-button-secondary uk-button-large uk-text-large">{letter}</button>
-                    )}
+                    { GetButtons() }
                 </div>
             </div>
         </div>
     );
+}
+
+function GetButtons() {
+    if(meetings.length === 0) {
+        return <span>No meetings scheduled</span>
+    }
+    return meetings.map((meeting, index) => <button key={index} className="uk-margin uk-button-secondary uk-button-large uk-text-large">{meeting}</button>);
 }
