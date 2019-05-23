eb63544fad8a6e926040e7d126cfb7d694f1b6c4
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-23 12:42:20 -0400

    Add base layout for estimate page

--- src/Estimate/Estimate.js
+++ src/Estimate/Estimate.js
@@ -5,11 +5,19 @@ export default function Estimate() {
     var points = "12345678";
     return(
       <>
-          <div className="uk-flex uk-flex-column uk-flex-middle">
-              <h1 className="uk-heading-medium uk-margin-top">Estimate</h1>
-              { Array.from(points).map((number,i) =>
-                  <button key={i} className="uk-margin uk-button-secondary uk-button-large uk-width-1-6 uk-text-large">{number}</button>)
-              }
+          <div className="uk-container uk-text-center@m uk-margin-top">
+              <h1 className="uk-heading-small uk-heading-divider">Estimate</h1>
+              <button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right">Host</button>
+              <dl className="uk-description-list">
+                  <dt># MBNA-10567: </dt>
+                  <dd>Add tooltip to my accounts page</dd>
+              </dl>
+              <div className="uk-align-center uk-width-1-2@m">
+                  { Array.from(points).map((number,i) =>
+                      <button key={i} className="uk-button uk-margin-small-top uk-button-primary uk-width-1-1">{number}</button>)
+                  }
+              </div>
+
           </div>
       </>
     );
