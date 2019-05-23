f295b52d727b83f8b772ad3962b7ce0883829ace
Ramandeep Aujla <aujlar@mac-3081.intware.com>
2019-05-23 09:27:37 -0400

    RKA fixed warning for unique "key" prop

--- src/Rooms/Rooms.js
+++ src/Rooms/Rooms.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function Rooms() {
     
-    var alphabet = "ABCD";
+    var alphabets = "ABCD";
     return (
         <div className="uk-flex uk-flex-column uk-flex-middle">
             <h1 className="uk-heading-medium uk-margin-top">ROOM</h1>
-            { Array.from(alphabet).map(letter => <button className="uk-margin uk-button-secondary uk-button-large uk-width-1-6 uk-text-large">{letter}</button>) }
+            { Array.from(alphabets).map((letter,i) => 
+                <button key={i} className="uk-margin uk-button-secondary uk-button-large uk-width-1-6 uk-text-large">{letter}</button>)
+            }
         </div>
     );
 }
