e227278c2c3ed19a0fd9a8f390fb9e9dc4fc2919
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-23 14:13:37 -0400

    Add toggle of active class

--- src/Estimate/Estimate.js
+++ src/Estimate/Estimate.js
@@ -4,21 +4,19 @@ export default function Estimate() {
 
     var points = "12345678";
     return(
-      <>
-          <div className="uk-container uk-text-center@m uk-margin-top">
-              <h1 className="uk-heading-small uk-heading-divider">Estimate</h1>
-              <button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right">Host</button>
-              <dl className="uk-description-list">
-                  <dt># MBNA-10567: </dt>
-                  <dd>Add tooltip to my accounts page</dd>
-              </dl>
-              <div className="uk-align-center uk-width-1-2@m">
-                  { Array.from(points).map((number,i) =>
-                      <button key={i} onClick={buttonClicked} className="uk-button uk-margin-small-top uk-button-primary uk-width-1-1 estimate-button">{number}</button>)
-                  }
-              </div>
+      <div className="uk-container uk-text-center@m uk-margin-top">
+          <h1 className="uk-heading-small uk-heading-divider">Estimate</h1>
+          <button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right">Host</button>
+          <dl className="uk-description-list">
+              <dt># MBNA-10567: </dt>
+              <dd>Add tooltip to my accounts page</dd>
+          </dl>
+          <div className="uk-align-center uk-width-1-2@m">
+              { Array.from(points).map((number,i) =>
+                  <button key={i} onClick={buttonClicked} className="uk-button uk-margin-small-top uk-button-primary uk-width-1-1 estimate-button">{number}</button>)
+              }
           </div>
-      </>
+      </div>
     );
 }
 
