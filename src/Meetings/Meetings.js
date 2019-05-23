c91592dedd9c023db10bd10634b82d849c8f21fc
Ramandeep Aujla <aujlar@mac-3081.intware.com>
2019-05-23 15:44:23 -0400

    RKA Changed Meetings to class instead of function

--- src/Meetings/Meetings.js
+++ src/Meetings/Meetings.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-const meetings = ['Meeting A', 'Meeting B', 'Meeting C', 'Meeting D', 'Meeting E'];
+export default class Meetings extends Component {
+    meetings = []; 
 
-export default function Meetings() {
-    
-    return (
-        <div className="uk-flex uk-flex-center">
-            <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <h1 className="uk-text-center uk-heading-divider">MEETINGS</h1>                
-                <div className="uk-flex uk-flex-column uk-flex-middle">
-                    { GetButtons() }
+    constructor() {
+        super();
+        this.meetings = ['Meeting A', 'Meeting B', 'Meeting C', 'Meeting D', 'Meeting E'];
+    }
+
+    get meetingButtons() {
+        if(this.meetings.length === 0) {
+            return <span>No meetings scheduled</span>
+        }
+        return this.meetings.map((meeting, index) => <Link to="/" key={index} className="uk-margin uk-button-secondary uk-button-large uk-text-large">{meeting}</Link>);    
+    }
+
+    render() {
+        return (
+            <div className="uk-flex uk-flex-center">
+                <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+                    <h1 className="uk-text-center uk-heading-divider">MEETINGS</h1>                
+                    <div className="uk-flex uk-flex-column uk-flex-middle">
+                        { this.meetingButtons }
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
-
-function GetButtons() {
-    if(meetings.length === 0) {
-        return <span>No meetings scheduled</span>
+        );
     }
-    return meetings.map((meeting, index) => <Link to="/" key={index} className="uk-margin uk-button-secondary uk-button-large uk-text-large">{meeting}</Link>);
+    
 }
--- src/Meetings/Meetings.test.js
+++ src/Meetings/Meetings.test.js
@@ -1,5 +1,23 @@
 import Meetings from "./Meetings";
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const fixture = new Meetings();
 
 it('Meetings renders correctly', () => {
-    expect(Meetings()).toMatchSnapshot(); 
+    expect(fixture.render()).toMatchSnapshot(); 
 });
+
+it('Meeting should return span element', () => {
+    fixture.meetings = [];
+    expect(fixture.meetingButtons).toEqual(<span>No meetings scheduled</span>);
+});
+
+// it('Meeting should return button elements', () => {
+//     fixture.meetings = ['A', 'B'];
+//     let expecetdButtons = [<Link to="/" className="uk-margin uk-button-secondary uk-button-large uk-text-large">A</Link>, <Link to="/" className="uk-margin uk-button-secondary uk-button-large uk-text-large">B</Link>];
+    
+//     expect(fixture.meetingButtons.length).toBe(2);
+//     expect(fixture.meetingButtons[0]).toBe(expecetdButtons[0]);
+//     expect(fixture.meetingButtons[1]).toEqual(expecetdButtons[1]);
+// });
