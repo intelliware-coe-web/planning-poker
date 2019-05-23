16ec0e855c91f71cfebf34c35dfc5287fb70581a
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-23 11:10:12 -0400

    Add estimate buttons

--- src/Estimate/Estimate.js
+++ src/Estimate/Estimate.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export default function Estimate() {
+
+    var points = "12345678";
     return(
       <>
           <div className="uk-flex uk-flex-column uk-flex-middle">
               <h1 className="uk-heading-medium uk-margin-top">Estimate</h1>
+              { Array.from(points).map((number,i) =>
+                  <button key={i} className="uk-margin uk-button-secondary uk-button-large uk-width-1-6 uk-text-large">{number}</button>)
+              }
           </div>
       </>
     );
