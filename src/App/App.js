8eee03e5094979e3c65f57d6d4404686064ab5d9
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-23 11:01:01 -0400

    Add route to estimate page, add test to estimate

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from 'react';
 import Login from './../Login/Login';
 import Rooms from './../Rooms/Rooms';
+import Estimate from './../Estimate/Estimate';
 
 export default function App() {
   
@@ -9,6 +10,7 @@ export default function App() {
     <Router>
       <Route exact path="/" component={Login}/>
       <Route path="/rooms" component={Rooms} />
+      <Route path="/estimate" component={Estimate} />
     </Router>
   );
 }
---
+++ src/Estimate/Estimate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Estimate() {
+    return(
+      <>
+          <div className="uk-flex uk-flex-column uk-flex-middle">
+              <h1 className="uk-heading-medium uk-margin-top">Estimate</h1>
+          </div>
+      </>
+    );
+}
