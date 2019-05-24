902abe18951a3d451545bc72de54d359d53eee4e
Ramandeep Aujla <aujlar@mac-3081.intware.com>
2019-05-24 10:57:51 -0400

    RKA added CreateUser page

--- src/App/App.js
+++ src/App/App.js
@@ -4,12 +4,13 @@ import Login from './../Login/Login';
 import Host from './../Host/Host'
 import Meetings from './../Meetings/Meetings';
 import Estimate from './../Estimate/Estimate';
+import CreateUser from "../User/CreateUser";
 
 export default function App() {
   
   return (
     <Router>
-      <Route exact path="/" component={Login}/>
+      <Route exact path="/" component={CreateUser}/>
       <Route path="/host" component={Host} />
       <Route path="/meetings/" component={Meetings} />
       <Route path="/estimate/" component={Estimate} />
