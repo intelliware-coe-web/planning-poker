2db413fa4435f7db8d33b4e067781ed2eca5b804
RatulK <ratulkhosla5@gmail.com>
2019-05-27 14:14:45 -0400

    RK - Adding Ticket Screen
    added title and updated styling for return link

--- src/Ticket/Ticket.js
+++ src/Ticket/Ticket.js
@@ -9,10 +9,11 @@ export default function Ticket() {
     }
 
     return (
-        <div className="uk-flex uk-flex-center">u
+        <div className="uk-flex uk-flex-center">
             <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+                <h3 className="uk-heading-medium uk-text-center">Add a Ticket</h3>
                 <a href="#/host/">
-                    <span uk-icon="icon: arrow-left; ratio: 2"></span>
+                    <span uk-icon="icon: arrow-left; ratio: 2" className="uk-position-small uk-position-top-left"></span>
                 </a>
                 <input placeholder="Ticket #" className="uk-input uk-margin uk-width-1-2@m uk-align-center" onChange={handleChange}></input>
                 <a href="#/host/" className="uk-width-1-2@m uk-align-center">
