b665a7423b4a4c6b7166329fbf9a1ccd51a22bd2
RatulK <ratulkhosla5@gmail.com>
2019-05-27 11:43:16 -0400

    RK - Adding Ticket Screen
    Adding Styling changes

--- src/Ticket/Ticket.js
+++ src/Ticket/Ticket.js
@@ -6,9 +6,9 @@ export default function Ticket() {
     return (
         <div className="uk-flex uk-flex-center">
             <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <h1 className="uk-heading-small">Add a Ticket</h1>
-                <input placeholder="Ticket #" className="uk-input uk-margin"></input>
-                <Link to="/host/" className="uk-button uk-button-primary">CREATE</Link>
+                <h1 className="uk-heading-small uk-text-center">Add a Ticket</h1>
+                <input placeholder="Ticket #" className="uk-input uk-margin uk-width-1-2@m uk-align-center"></input>
+                <Link to="/host/" className="uk-button uk-button-primary uk-width-1-2@m uk-align-center">CREATE</Link>
             </div>
         </div>
     );
