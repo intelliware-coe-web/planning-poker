b10797117fee961029b27e1d188abc84abca99b9
Linna Meng <mengl@mac-2387.local>
2019-05-27 10:33:45 -0400

    LM update styling to be consistent

--- src/Meetings/Meetings.js
+++ src/Meetings/Meetings.js
@@ -6,25 +6,39 @@ const meetings = ['Meeting A', 'Meeting B', 'Meeting C', 'Meeting D', 'Meeting E
 export default function Meetings() {
     
     return (
-        <div className="uk-flex uk-flex-center">
-            <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <h1 className="uk-text-center uk-heading-divider">MEETINGS</h1>                
-                <div className="uk-flex uk-flex-column uk-flex-middle">
-                    { GetButtons() }
-                    { GetAddMeetingButton() }
-                </div>
+        <div className="uk-container uk-text-center@m uk-margin-top">
+            <h1 className="uk-heading-small uk-heading-divider">Meetings</h1>
+            <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-1">
+                { GetMeetingButtons() }
+                { GetAddMeetingButton() }
             </div>
         </div>
     );
 }
 
-function GetButtons() {
+function GetMeetingButtons() {
     if (meetings.length === 0) {
         return <span>No meetings scheduled</span>
     }
-    return meetings.map((meeting, index) => <Link to="/" key={index} className="uk-margin uk-button-secondary uk-button-large uk-text-large">{meeting}</Link>);
+    return meetings.map((meeting, index) => 
+        <button className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
+            <Link to="/host" key={index} >
+                <div className="uk-card uk-card-secondary uk-card-body">
+                    <h3 className="uk-card-title">{meeting}</h3>
+                </div>
+            </Link>
+        </button>
+    );
 }
 
 function GetAddMeetingButton() {
-    return <Link to="/meetings/add" className="uk-margin uk-button-secondary uk-button-large uk-text-large">+</Link>;
+    return (
+        <button className="uk-margin uk-button-secondary uk-width-1-2 uk-text-large">
+            <Link to="/meetings/add">
+                <div className="uk-card uk-card-secondary uk-card-body">
+                    <h3 className="uk-card-title">+</h3>
+                </div>
+            </Link>
+        </button>
+    );
 }
