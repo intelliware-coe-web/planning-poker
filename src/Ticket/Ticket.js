455f7c90f0e107fbc5c5ce19e19c0481aabe01f8
RatulK <ratulkhosla5@gmail.com>
2019-05-27 13:55:34 -0400

    RK - Adding Ticket Screen
    added return arrow

--- src/Ticket/Ticket.js
+++ src/Ticket/Ticket.js
@@ -11,7 +11,9 @@ export default function Ticket() {
     return (
         <div className="uk-flex uk-flex-center">
             <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <h1 className="uk-heading-small uk-text-center">Add a Ticket</h1>
+                <a href="#/host/">
+                    <span uk-icon="icon: arrow-left; ratio: 2"></span>
+                </a>
                 <input placeholder="Ticket #" className="uk-input uk-margin uk-width-1-2@m uk-align-center" onChange={handleChange}></input>
                 <a href="#/host/" className="uk-width-1-2@m uk-align-center">
                     <button className="uk-button uk-button-primary uk-width-1-1@m" disabled={'' === ticketNumber}>CREATE</button>
