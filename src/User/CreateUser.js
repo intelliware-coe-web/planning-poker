fd7a9e8098b47f3bbe7470fc0f88ec51c2c90531
Linna Meng <mengl@mac-2387.local>
2019-05-28 12:07:10 -0400

    LM inline button

--- src/User/CreateUser.js
+++ src/User/CreateUser.js
@@ -6,14 +6,15 @@ import styles from './CreateUser.css';
 export default function CreateUser() {
     return (
         <div class="uk-flex uk-flex-middle uk-height-viewport login-background">
-            <div class="uk-container uk-width-1-2 uk-margin-small">
-                <h1>Let's get started</h1> 
-                <i><h3>create a new user</h3></i>            
-                <div class="uk-inline uk-width-1-1 uk-margin">
+            <div class="uk-container uk-width-1-1 uk-margin-small">
+                <h1>Welcome to planning poker</h1> 
+                <i><h3>Create a new user to get started!</h3></i>            
+                <div class="uk-inline uk-width-1-2 uk-margin">
                     <span class="uk-form-icon" uk-icon="icon: user"></span>
                     <input class="uk-input" placeholder="Name" type="text"/>
                 </div>
-                <Link to="/meetings/" className="uk-button uk-button-primary">CREATE</Link>
+                <Link to="/meetings/" className="uk-button uk-button-primary uk-width-1-5">CREATE</Link>
+
             </div>
             <div class="uk-container uk-width-1-1">
                 <img src={rocket} alt="logo" height="20vmin" data-uk-img/>
