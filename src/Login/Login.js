67159f98ea7cad53977210714e71d56af2bbec53
Linna Meng <mengl@mac-2387.local>
2019-05-28 11:37:50 -0400

    LM test rocket theme

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../resources/images/logo.png';
+import logo from '../resources/images/login.png';
 import {Link} from 'react-router-dom';
 
 export default function Login() {
@@ -7,7 +7,7 @@ export default function Login() {
     return (
         <div className="uk-flex uk-flex-center">
             <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <img src={logo} alt="logo" height="40vmin" data-uk-img className="uk-animation-kenburns uk-align-center"/>
+                <img src={login} alt="logo" height="40vmin" data-uk-img className="uk-animation-kenburns uk-align-center"/>
                 <h1>Planning Poker</h1>                
                 <input placeholder="EMAIL" className="uk-input uk-margin-top" type="email"></input>
                 <input placeholder="PASSWORD" className="uk-input uk-margin" type="password"></input>
--- src/User/CreateUser.js
+++ src/User/CreateUser.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import rocket from '../resources/images/rocket.jpg';
+import styles from './CreateUser.css';
 
 export default function CreateUser() {
     return (
-        <div className="uk-flex uk-flex-center">
-            <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-                <h1 className="uk-heading-small">Create New User</h1>                
-                <input placeholder="Name" className="uk-input uk-margin"></input>
+        <div class="uk-flex uk-flex-middle uk-height-viewport login-background">
+            <div class="uk-container uk-width-1-2 uk-margin-small">
+                <h1>Let's get started</h1> 
+                <i><h3>create a new user</h3></i>            
+                <div class="uk-inline uk-width-1-1 uk-margin">
+                    <span class="uk-form-icon" uk-icon="icon: user"></span>
+                    <input class="uk-input" placeholder="Name" type="text"/>
+                </div>
                 <Link to="/meetings/" className="uk-button uk-button-primary">CREATE</Link>
+            </div>
+            <div class="uk-container uk-width-1-1">
+                <img src={rocket} alt="logo" height="20vmin" data-uk-img/>
             </div>
         </div>
     );
