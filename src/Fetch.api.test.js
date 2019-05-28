6e7156ceeda03ea92bc5818fa3eab1186ae7e3b7
Ramandeep Aujla <aujlar@mac-3081.intware.com>
2019-05-28 12:14:00 -0400

    RA refactored tests for FetchApi

--- src/Fetch.api.test.js
+++ src/Fetch.api.test.js
@@ -3,26 +3,38 @@ import {getData, postData} from './Fetch.api';
 describe('FetchApi', () => {
     const url = 'http://test.test';
 
-    describe('getData', () => {        
+    describe('getData', () => {  
         it('should return data when server returns ok response', (done) => {
-            mockFetchReturningSuccessResponse();
+            const SUCCESS_RESPONSE = {
+                ok: true,
+                status: 200,
+                json: jest.fn()
+            };
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve(SUCCESS_RESPONSE));
+
             getData(url).then(response => {
-                expect(SUCCESS_RESPONSE.json).toHaveBeenCalled();
+                expect(SUCCESS_RESPONSE.json).toHaveBeenCalledTimes(1);
                 done();               
             });
         });
 
         it('should throw error when server returns not ok response', (done) => {
-            mockFetchReturningErrorResponse();
+            const ERROR_RESPONSE = {
+                ok: false,
+                status: 404,
+                json: jest.fn()
+            };
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve(ERROR_RESPONSE));
+
             getData(url).catch(error => {
-                expect(SUCCESS_RESPONSE.json).toHaveBeenCalled();
+                expect(ERROR_RESPONSE.json).toHaveBeenCalledTimes(0);
                 expect(error.message).toBe('HTTP status ' + ERROR_RESPONSE.status);
                 done();               
             });
         });
     
         it('should throw error when server throws an error', (done) => {
-            mockFetchThrowingError(); 
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject());
             getData(url).catch(error => {
                 expect(error.message).toBe('Get data failed from url ' + url);
                 done();
@@ -31,26 +43,39 @@ describe('FetchApi', () => {
     });
 
     describe('postData', () => { 
-        const data = {};       
+        const data = {};
+
         it('should return data when server returns ok response', (done) => {
-            mockFetchReturningSuccessResponse();
+            const SUCCESS_RESPONSE = {
+                ok: true,
+                status: 200,
+                json: jest.fn()
+            };
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve(SUCCESS_RESPONSE));
+
             postData(url, data).then(response => {
-                expect(SUCCESS_RESPONSE.json).toHaveBeenCalled();
+                expect(SUCCESS_RESPONSE.json).toHaveBeenCalledTimes(1);
                 done();               
             });
         });
 
         it('should throw error when server returns not ok response', (done) => {
-            mockFetchReturningErrorResponse();
+            const ERROR_RESPONSE = {
+                ok: false,
+                status: 404,
+                json: jest.fn()
+            };
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve(ERROR_RESPONSE));
+
             postData(url, data).catch(error => {
-                expect(SUCCESS_RESPONSE.json).toHaveBeenCalled();
+                expect(ERROR_RESPONSE.json).toHaveBeenCalledTimes(0);
                 expect(error.message).toBe('HTTP status ' + ERROR_RESPONSE.status);
                 done();               
             });
         });
     
         it('should throw error when server throws an error', (done) => {
-            mockFetchThrowingError(); 
+            jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject()); 
             postData(url, data).catch(error => {
                 expect(error.message).toBe('Post data failed to url ' + url);
                 done();
@@ -58,29 +83,3 @@ describe('FetchApi', () => {
         });
     });    
 });
-
-const SUCCESS_RESPONSE = {
-    ok: true,
-    status: 200,
-    json: jest.fn()
-}
-
-const ERROR_RESPONSE = {
-    ok: false,
-    status: 404,
-    json: jest.fn()
-}
-
-function mockFetchReturningSuccessResponse() {
-    const mockFetchPromise = Promise.resolve(SUCCESS_RESPONSE);
-    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
-}
-
-function mockFetchReturningErrorResponse() {
-    const mockFetchPromise = Promise.resolve(ERROR_RESPONSE);
-    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
-}
-
-function mockFetchThrowingError() {
-    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject());
-}
