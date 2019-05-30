6f587cd53254691a8119e1916a70c9adbd26fd9a
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-30 11:26:16 -0400

    Introduce saga

--- src/store.js
+++ src/store.js
@@ -3,14 +3,19 @@ import thunk from 'redux-thunk';
 import {createHashHistory} from 'history';
 import createRootReducer from './RootReducer';
 import {routerMiddleware} from "connected-react-router";
+import createSagaMiddleware from 'redux-saga';
 
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const history = createHashHistory();
 export default function store() {
     return createStore(
         createRootReducer(history),
-        composeEnhancers(applyMiddleware(
-            routerMiddleware(history),
-            thunk))
+        composeEnhancers(
+            applyMiddleware(
+                routerMiddleware(history),
+                thunk,
+                sagaMiddleware
+        ))
     );
 };
