e0bebafcafffa3bd402cc46b4de93e26d3b1558e
mr-rampage <readonly@che.wbac.ca>
2019-06-04 12:58:11 -0400

    test(withHeader): adding test coverage

--- src/Common/Header.test.js
+++ src/Common/Header.test.js
@@ -9,5 +9,9 @@ describe('Higher Order Component: withHeader', () => {
 
   it('should render a Header on a component correctly', () => {
     expect(withHeader(FakeComponent, 'Fake Component')({ onBack: noop })).toMatchSnapshot();
-  })
+  });
+
+  it('should not render the back button when onBack is not provided', () => {
+    expect(withHeader(FakeComponent, 'Fake Component')({})).toMatchSnapshot();
+  });
 });
