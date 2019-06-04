615245120c0c54e11e29ba324af213b10591f357
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-06-04 17:15:47 -0400

    Add tests for error reducer

--- src/ErrorReducer.spec.js
+++ src/ErrorReducer.spec.js
@@ -1,6 +1,9 @@
 import ErrorReducer from './ErrorReducer';
-import { MEETINGS_ERROR } from './Meetings/Actions/MeetingsActions';
-import { USER_ERROR } from './User/Actions/UserActions';
+import {MEETINGS_ERROR} from './Meetings/Actions/MeetingsActions';
+import {USER_ERROR} from './User/Actions/UserActions';
+import {CURRENT_MEETING_ERROR} from "./Meetings/Actions/CurrentMeetingActions";
+import {ESTIMATE_ERROR} from "./Estimate/Actions/EstimateActions";
+import {STORIES_ERROR} from "./Stories/Actions/StoriesActions";
 
 describe('Error Reducer ', () => {
 
@@ -20,6 +23,33 @@ describe('Error Reducer ', () => {
 
     const actualNextState = ErrorReducer(initialState, action);
     expect(actualNextState).toEqual({error});
+  });
+
+  it('should set error on a current meeting error', () => {
+      const initialState = null;
+      const error = "Page Not Found";
+      const action = { type: CURRENT_MEETING_ERROR, payload: { error: error } };
+
+      const actualNextState = ErrorReducer(initialState, action);
+      expect(actualNextState).toEqual({error});
+  });
+
+  it('should set error on a estimate error', () => {
+      const initialState = null;
+      const error = "Page Not Found";
+      const action = { type: ESTIMATE_ERROR, payload: { error: error } };
+
+      const actualNextState = ErrorReducer(initialState, action);
+      expect(actualNextState).toEqual({error});
+  });
+
+  it('should set error on a stories error', () => {
+      const initialState = null;
+      const error = "Page Not Found";
+      const action = { type: STORIES_ERROR, payload: { error: error } };
+
+      const actualNextState = ErrorReducer(initialState, action);
+      expect(actualNextState).toEqual({error});
   });
 
   it('should reset the error to the initial state on non-error messages', () => {
