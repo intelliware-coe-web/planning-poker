07032f9a34e1e25af61dff3478a1794bd3a22bca
Anson Chung <anson.chung@gmail.com>
2019-06-05 11:23:13 -0400

    fixing console error in test and deleting enzyme as we are not using it

--- src/Common/Header.test.js
+++ src/Common/Header.test.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 describe('Higher Order Component: withHeader', () => {
   it('should render a Header on a component correctly', () => {
-    expect(Page({ title: 'Foo', onBack: noop, children:[<div/>, <div/>] })).toMatchSnapshot();
+    expect(Page({ title: 'Foo', onBack: noop, children:[<div key="1"></div>, <div key="2"></div>] })).toMatchSnapshot();
   });
 
   it('should not render the back button when onBack is not provided', () => {
