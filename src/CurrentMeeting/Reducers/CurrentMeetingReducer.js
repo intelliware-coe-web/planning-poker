1ec5cc3fb35fd6800462e2a526b3447ca82f6096
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-06-05 14:52:30 -0400

    Add current meeting to Redux, insert meeting name into estimate page head

--- src/CurrentMeeting/Reducers/CurrentMeetingReducer.js
+++ src/CurrentMeeting/Reducers/CurrentMeetingReducer.js
@@ -1,6 +1,15 @@
 import { CURRENT_MEETING_SUCCESS } from "../Actions/CurrentMeetingActions";
 
-export default function CurrentMeetingReducer(state = null, action) {
+const initialState = {
+    "attendees": [],
+    "_id": null,
+    "name": "Loading Meeting...",
+    "created_date": null,
+    "__v": null,
+    "current_story": null
+};
+
+export default function CurrentMeetingReducer(state = initialState, action) {
     if (action.type === CURRENT_MEETING_SUCCESS ) {
         return action.payload.currentMeeting;
     } else {
--- src/Estimate/Components/Estimate.js
+++ src/Estimate/Components/Estimate.js
@@ -4,12 +4,12 @@ import { estimateStory } from '../Actions/EstimateActions';
 import { viewHost, viewMeetings } from '../../Navigation/route-actions';
 import { Page } from '../../Common/Header';
 
-export function Estimate({ story, meeting, user, goToMeetings, goToHost, estimateStory }) {
+export function Estimate({ story, currentMeeting, user, goToMeetings, goToHost, estimateStory }) {
   // TODO: Move estimations to store
   const estimation = [1, 2, 3, 5, 8, 13];
   return (
-    <Page title='Estimate' onBack={ goToMeetings }>
-      <HostButton hasHost={ meeting && !!meeting.host } onHostClick={ goToHost }/>
+    <Page title={currentMeeting.name} onBack={ goToMeetings }>
+      <HostButton hasHost={ currentMeeting && !!currentMeeting.host } onHostClick={ goToHost }/>
       <StoryDescription { ...story } />
       <div className="uk-align-center uk-width-1-1@m">
         { estimation.map((estimate, i) =>
@@ -45,7 +45,7 @@ function HostButton({ hasHost, onHostClick }) {
 function mapStateToProps(state) {
   return {
       story: state.estimateStory,
-      meeting: state.currentMeeting,
+      currentMeeting: state.currentMeeting,
       user: state.user
   }
 }
