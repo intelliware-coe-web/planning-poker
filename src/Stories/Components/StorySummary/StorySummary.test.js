7939dda86a44ad06297aeac4f2699b3cff926fd1
Anson Chung <anson.chung@gmail.com>
2019-06-11 13:20:00 -0400

    showing user names and estimates

--- src/Stories/Components/StorySummary/StorySummary.test.js
+++ src/Stories/Components/StorySummary/StorySummary.test.js
@@ -4,9 +4,39 @@ it('StorySummary renders correctly', () => {
     const props = {
         currentMeeting: jest.fn().mockReturnValue({
             name: 'Mock Meeting Name'
-          }),
-          
-          goToStories: jest.fn()
+        }),
+        storyEstimates: [], 
+        goToStories: jest.fn()
+    }
+    expect(StorySummary(props)).toMatchSnapshot();
+});
+
+it('StorySummary renders estimates withs names', () => {
+    const props = {
+        currentMeeting: jest.fn().mockReturnValue({
+            name: 'Mock Meeting Name'
+        }),
+        storyEstimates: [{
+            user: {
+                name: 'Bob'
+            },
+            estimate: 1
+        },
+        {
+            user: {
+                name: 'Andy'
+            },
+            estimate: 5
+        },
+        {
+            user: {
+                name: 'Joe'
+            },
+            estimate: 3
+        },
+        {}
+        ], 
+        goToStories: jest.fn()
     }
     expect(StorySummary(props)).toMatchSnapshot();
 });
