e11667c8ce8cf33135ef67aed0de79b149d66019
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-06-12 10:31:54 -0400

    Add react-testing-library dependency, update story estimate test

--- src/StoryEstimate/Components/StoryEstimate.js
+++ src/StoryEstimate/Components/StoryEstimate.js
@@ -5,70 +5,70 @@ import {viewMeetings, viewStories} from '../../Navigation/route-actions';
 import {Page} from '../../Common/Page';
 import {GetCurrentStory, StopCurrentStoryPolling} from "../../CurrentStory/Actions/CurrentStoryActions";
 
-export function StoryEstimate({ storyEstimate, currentMeeting, user, currentStory, goToMeetings, goToStories, updateStoryEstimate, resetStoryEstimate, getCurrentStory, stopCurrentStoryPolling, match }) {
-  // TODO: Move estimations to store
-  const estimation = [1, 2, 3, 5, 8, 13];
+export function StoryEstimate({storyEstimate, currentMeeting, user, currentStory, goToMeetings, goToStories, updateStoryEstimate, resetStoryEstimate, getCurrentStory, stopCurrentStoryPolling, match}) {
+    // TODO: Move estimations to store
+    const estimation = [1, 2, 3, 5, 8, 13];
 
-  useEffect(
-      () => {
-          getCurrentStory(match.params.meetingId);
-          return () => {
-              stopCurrentStoryPolling();
-              resetStoryEstimate();
-          };
-      },
-      [getCurrentStory, stopCurrentStoryPolling, resetStoryEstimate, match],
-  );
+    useEffect(
+        () => {
+            getCurrentStory(match.params.meetingId);
+            return () => {
+                stopCurrentStoryPolling();
+                resetStoryEstimate();
+            };
+        },
+        [getCurrentStory, stopCurrentStoryPolling, resetStoryEstimate, match],
+    );
 
-  return (
-    <Page title={currentMeeting.name} onBack={ goToMeetings }>
-      <HostButton onHostClick={ goToStories }/>
-      <StoryDescription { ...currentStory } />
-      <div className="uk-align-center uk-width-1-1@m">
-        { estimation.map((estimate, i) =>
-          <button key={ i } onClick={ () => updateStoryEstimate(user._id, currentStory._id, estimate) }
-                  className={ `uk-button uk-margin-small-top uk-width-1-1 uk-inline pp-button ${ storyEstimate === estimate ? 'selected' : '' }` }>
-            { estimate }
-            <span
-              className={ 'uk-position-center-right uk-background-muted uk-text-emphasis uk-label uk-margin-small-right' }
-              hidden={ storyEstimate !== estimate }>Selected</span>
-          </button>)
-        }
-      </div>
-    </Page>
-  );
+    return (
+        <Page title={currentMeeting.name} onBack={goToMeetings}>
+            <HostButton onHostClick={goToStories}/>
+            <StoryDescription {...currentStory} />
+            <div className="uk-align-center uk-width-1-1@m">
+                {estimation.map((estimate, i) =>
+                    <button key={i} onClick={() => updateStoryEstimate(user._id, currentStory._id, estimate)}
+                            className={`uk-button uk-margin-small-top uk-width-1-1 uk-inline pp-button ${ storyEstimate === estimate ? 'selected' : '' }`}>
+                        {estimate}
+                        <span
+                            className={'uk-position-center-right uk-background-muted uk-text-emphasis uk-label uk-margin-small-right'}
+                            hidden={storyEstimate !== estimate}>Selected</span>
+                    </button>)
+                }
+            </div>
+        </Page>
+    );
 }
 
-function StoryDescription({ name, description }) {
-  return (<dl className="uk-description-list">
-    <dt># { name } :</dt>
-    <dd>{ description }</dd>
-  </dl>);
+function StoryDescription({name, description}) {
+    return (<dl className="uk-description-list">
+        <dt># {name} :</dt>
+        <dd>{description}</dd>
+    </dl>);
 }
 
-function HostButton({ onHostClick }) {
-  return <button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right"
-                  onClick={ onHostClick }>Host</button>;
+function HostButton({onHostClick}) {
+    return <button className="uk-button uk-button-primary uk-button-small uk-position-small uk-position-top-right"
+                   onClick={onHostClick}>Host</button>;
 }
 
 function mapStateToProps(state) {
-  return {
-    storyEstimate: state.storyEstimate,
-    currentMeeting: state.currentMeeting,
-    user: state.user,
-    currentStory: state.currentStory
-  }
+    return {
+        storyEstimate: state.storyEstimate,
+        currentMeeting: state.currentMeeting,
+        user: state.user,
+        currentStory: state.currentStory
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    goToMeetings: () => dispatch(viewMeetings()),
-    goToStories: () => dispatch(viewStories()),
-    updateStoryEstimate: (userId, storyId, estimate) => dispatch(PutStoryEstimate(userId, storyId, estimate)),
-    resetStoryEstimate: () => dispatch(ResetStoryEstimate()),
-    getCurrentStory: (meetingId) => dispatch(GetCurrentStory(meetingId)),
-    stopCurrentStoryPolling: () => dispatch(StopCurrentStoryPolling()),
-  };
+    return {
+        goToMeetings: () => dispatch(viewMeetings()),
+        goToStories: () => dispatch(viewStories()),
+        updateStoryEstimate: (userId, storyId, estimate) => dispatch(PutStoryEstimate(userId, storyId, estimate)),
+        resetStoryEstimate: () => dispatch(ResetStoryEstimate()),
+        getCurrentStory: (meetingId) => dispatch(GetCurrentStory(meetingId)),
+        stopCurrentStoryPolling: () => dispatch(StopCurrentStoryPolling()),
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(StoryEstimate)
--- src/StoryEstimate/Components/StoryEstimate.test.js
+++ src/StoryEstimate/Components/StoryEstimate.test.js
@@ -1,28 +1,28 @@
-import { StoryEstimate } from './StoryEstimate';
+import React from 'react'
+import {StoryEstimate} from './StoryEstimate';
+import {cleanup, render} from '@testing-library/react'
 
 describe('Container: Story Estimate', () => {
-  it('should render Estimate correctly', () => {
-    const props = {
-      storyEstimate: 1,
-      goToMeetings: jest.fn(),
-      goToStories: jest.fn(),
-      updateStoryEstimate: jest.fn(),
-      resetStoryEstimate: jest.fn(),
-      user: jest.fn(),
-      currentStory: jest.fn(),
-      getCurrentStory: jest.fn(),
-      stopCurrentStoryPolling: jest.fn(),
-      match: jest.fn().mockReturnValue({
-          params: {
-            meetingId: 'Mock meeting id'
-          }
-      }),
-      currentMeeting: jest.fn().mockReturnValue({
-        name: 'Mock Meeting Name'
-      })
-    };
 
-    // FIXME: Test broken due to useEffect
-    // expect(StoryEstimate(props)).toMatchSnapshot();
-  })
+    afterEach(cleanup);
+
+    it('should render Estimate correctly', () => {
+        const props = {
+            storyEstimate: 1,
+            goToMeetings: jest.fn(),
+            goToStories: jest.fn(),
+            updateStoryEstimate: jest.fn(),
+            resetStoryEstimate: jest.fn(),
+            user: jest.fn(),
+            currentStory: jest.fn(),
+            getCurrentStory: jest.fn(),
+            stopCurrentStoryPolling: jest.fn(),
+            currentMeeting: jest.fn().mockReturnValue({
+                name: 'Mock Meeting Name'
+            })
+        };
+
+        const {container}  = render(<StoryEstimate  {...props} match={{'params': { 'meetingId': 'Mock meeting id'}}}/>);
+        expect(container).toMatchSnapshot();
+    });
 });
