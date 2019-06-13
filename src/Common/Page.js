fee229b0c1ed07709020bf3e8a5a642fc782008b
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-06-13 14:03:15 -0400

    Modify page to contain dynamic font, section above header title

--- src/Common/Page.js
+++ src/Common/Page.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-function BackButton({ goBack }) {
-  return goBack ?
-    (<span data-uk-icon="icon: arrow-left; ratio: 3" className="uk-position-large uk-position-top-left"
-           onClick={ goBack }/>) :
-    (<></>);
+function BackButton({goBack}) {
+    return goBack ?
+        (<span data-uk-icon="icon: arrow-left; ratio: 1.75" className="uk-position-small uk-position-top-left"
+               onClick={goBack}/>) :
+        (<></>);
 }
 
-export function Page({ title, onBack, children }) {
-  return (
-    <div className="default-background">
-      <div className="uk-container uk-text-center@m">
-        <h1 className="uk-heading-divider uk-margin-top">{ title }</h1>
-        <div className="uk-align-center uk-width-1-2@m">
-          <BackButton goBack={ onBack }/>
-          {children}
+export function Page({title, onBack, children}) {
+    return (
+        <div className="default-background">
+            <div className="uk-container uk-text-center@m">
+                <div className="uk-section uk-section-xsmall"><BackButton goBack={onBack}/></div>
+                <h1 className="uk-heading-divider uk-margin-small-top">{title}</h1>
+                <div className="uk-align-center uk-width-1-2@m">
+                    {children}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
