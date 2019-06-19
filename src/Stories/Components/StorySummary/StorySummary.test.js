d88527bb86d1f0012a93ecff6d3b3b225cd95628
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-06-19 13:39:23 -0400

    Remove obsolete test

--- src/Stories/Components/StorySummary/StorySummary.test.js
+++ src/Stories/Components/StorySummary/StorySummary.test.js
@@ -2,35 +2,22 @@ import {StorySummary} from "./StorySummary";
 import {cleanup, render} from "@testing-library/react";
 import React from "react";
 
-describe('Container: StorySummary', ()=> {
+describe('Container: StorySummary', () => {
 
     afterEach(cleanup);
-    it('should render correctly', () => {
-        const props = {
-            currentMeeting: jest.fn().mockReturnValue({
-                name: 'Mock Meeting Name'
-            }),
-            storyEstimates: [],
-            goToStories: jest.fn(),
-            updateCurrentStory: jest.fn(),
-            getStoryEstimates: jest.fn(),
-            stopStoryEstimatesPolling: jest.fn()
-        };
-        const {container} = render(<StorySummary  {...props} match={{'params': {'storyId': 'Mock story id'}}}/>);
-        expect(container).toMatchSnapshot();
-    });
 
-    it('should renders estimates withs names', () => {
+    it('should render correctly with estimates and names', () => {
         const props = {
             currentMeeting: jest.fn().mockReturnValue({
                 name: 'Mock Meeting Name'
             }),
-            storyEstimates: [{
-                user: {
-                    name: 'Bob'
+            storyEstimates: [
+                {
+                    user: {
+                        name: 'Bob'
+                    },
+                    estimate: 1
                 },
-                estimate: 1
-            },
                 {
                     user: {
                         name: 'Andy'
