6e3a3cf534ecfec103a27403b6df9475b18bdc18
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-21 20:12:14 +0000

    Deploy intelliware-coe-web/planning-poker to github.com/intelliware-coe-web/planning-poker.git:gh-pages

---
+++ precache-manifest.0b9b325fd8b810aa499777dadbf6b444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fbb284b9a73257a9ad8910dad7bf62",
+    "url": "/planning-poker/index.html"
+  },
+  {
+    "revision": "e4e8b29da32264ff4dd2",
+    "url": "/planning-poker/static/css/main.3893eab5.chunk.css"
+  },
+  {
+    "revision": "bd743fce5e771d742276",
+    "url": "/planning-poker/static/js/2.f3cef131.chunk.js"
+  },
+  {
+    "revision": "e4e8b29da32264ff4dd2",
+    "url": "/planning-poker/static/js/main.0257b988.chunk.js"
+  },
+  {
+    "revision": "5616061573c6ca61febf",
+    "url": "/planning-poker/static/js/runtime~main.b5575156.js"
+  },
+  {
+    "revision": "5658e2156a35b0e92a2ea55b865454b8",
+    "url": "/planning-poker/static/media/pp_logo_transaparent.5658e215.png"
+  }
+]);
